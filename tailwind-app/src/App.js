305530ef4657b78109f14e7037a4dc0040639036
yksl <yukselkurtuldu9@gmail.com>
2024-04-19 11:07:58 +0300

    edit

--- tailwind-app/src/App.js
+++ tailwind-app/src/App.js
@@ -1,37 +1,70 @@
+
+
 function App() {
   return (
     <div>
-      <header >
-        <div>
-          <div className="flex justify-between mt-4">
-            <div className="flex gap-10 ms-5 items-center">
-              <a href="#clarusway">
-                <img
-                  src="https://d33wubrfki0l68.cloudfront.net/682a555ec15382f2c6e7457ca1ef48d8dbb179ac/f8cd3/images/logo.svg"
-                  alt="logo"
-                />
-              </a>
-            <div className="flex gap-3 items-center">
-              <a className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg" href="#solutions"> Solutions </a>
-              <a className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg" href="#industries"> Industries </a>
-              <a className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg" href="#fees"> Fees </a>
-              <a className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg" href="#About"> About Rareblocks </a>
-            </div>
-            </div>
-            <div className="flex gap-3 items-center me-5">
-              <a className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg" href="#login"> Sign in </a>
-              <a className=" w-[187px] h-[35px] bg-gray-900 text-white justify-center flex items-center rounded-lg" href="#register" rel="noopener noreferrer" role="button">
-                Create free account
-              </a>
-            </div>
+      <header className="flex justify-between mt-5">
+        <div className=" ms-10 items-center">
+          <a href="#clarusway">
+            <img
+              src="https://d33wubrfki0l68.cloudfront.net/682a555ec15382f2c6e7457ca1ef48d8dbb179ac/f8cd3/images/logo.svg"
+              alt="logo"
+            />
+          </a>
+        </div>
+        <div className="flex justify-between w-10/12">
+          <div className="flex gap-3 items-center ">
+            <a
+              className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg"
+              href="#solutions"
+            >
+              Solutions
+            </a>
+            <a
+              className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg"
+              href="#industries"
+            >
+              Industries
+            </a>
+            <a
+              className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg"
+              href="#fees"
+            >
+              Fees
+            </a>
+            <a
+              className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg"
+              href="#About"
+            >
+              About Rareblocks
+            </a>
+          </div>
+          <div className="flex gap-3 items-center me-10">
+            <a
+              className="hover:bg-slate-700 hover:text-white py-1 px-3 rounded-lg"
+              href="#login"
+            >
+              {" "}
+              Sign in{" "}
+            </a>
+            <a
+              className=" w-[187px] h-[35px] bg-gray-900 text-white justify-center flex items-center rounded-lg"
+              href="#register"
+              rel="noopener noreferrer"
+              role="button"
+            >
+              Create free account
+            </a>
           </div>
         </div>
       </header>
       <section>
-        <div className="flex justify-evenly items-center mt-5 max-md:flex-col">
+        <div className="flex justify-evenly items-center mt-10 max-md:flex-col">
           <div className=" w-[481px] flex flex-col max-md:w-80">
             <div>
-              <h1 className="font-plus-jakarta-sans text-[58px] leading-[66px] text-gray-900 font-extrabold max-md:text-center">A special credit card made for Developers.</h1>
+              <h1 className="font-plus-jakarta-sans text-[58px] leading-[66px] text-gray-900 font-extrabold max-md:text-center max-md:text-[40px] max-md:leading-[46px]">
+                A special credit card made for Developers.
+              </h1>
               <p className="my-6 font-Inter-sans text-[16px] leading-[26px] text-gray-600 max-md:text-center">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vehicula massa in enim luctus. Rutrum arcu.
@@ -45,15 +78,22 @@ function App() {
                     required
                   />
                   <div className="max-md:m-auto max-md:mt-4">
-                    <button className="p-3 bg-gray-900 text-white rounded-xl max-md:w-48 " type="submit">Get Free Card</button>
+                    <button
+                      className="p-3 bg-gray-900 text-white rounded-xl max-md:w-48 "
+                      type="submit"
+                    >
+                      Get Free Card
+                    </button>
                   </div>
                 </div>
               </form>
             </div>
             <div className="flex justify-between items-center ">
               <div className="flex justify-between items-center gap-3">
-                <p className="font-plus-jakarta-sans font-medium text-[42px] leading-[62px] max-md:text-[28px]">2943</p>
-                <p className="font-plus-jakarta-sans text-[13px] leading-[20px]"> 
+                <p className="font-plus-jakarta-sans font-medium text-[42px] leading-[62px] max-md:text-[28px]">
+                  2943
+                </p>
+                <p className="font-plus-jakarta-sans text-[13px] leading-[20px]">
                   Cards
                   <br />
                   Delivered
@@ -61,7 +101,7 @@ function App() {
               </div>
               <div className=" max-md:invisible">
                 <svg
-                className="text-gray-300"
+                  className="text-gray-300"
                   width={16}
                   height={39}
                   viewBox="0 0 16 39"
@@ -77,7 +117,9 @@ function App() {
                 </svg>
               </div>
               <div className="flex justify-between items-center gap-3">
-                <p className="font-plus-jakarta-sans font-medium text-[42px] leading-[62px] max-md:text-[28px]">$1M+</p>
+                <p className="font-plus-jakarta-sans font-medium text-[42px] leading-[62px] max-md:text-[28px]">
+                  $1M+
+                </p>
                 <p className="font-plus-jakarta-sans text-[13px] leading-[20px]">
                   Transaction
                   <br />
@@ -87,11 +129,16 @@ function App() {
             </div>
           </div>
           <div className="flex justify-end w-[554px] ">
-            <img className="h-[38rem] max-md:hidden"
+            <img
+              className="h-[38rem] max-md:hidden"
               src="https://d33wubrfki0l68.cloudfront.net/d6f1462500f7670e0db6b76b35054a081679a5a0/0ce15/images/hero/5.1/illustration.png"
               alt="images"
             />
-            <img className=" hidden max-md:block max-md:m-auto" src="./image.png" alt="image" />
+            <img
+              className=" hidden max-md:block max-md:m-auto"
+              src="./image.png"
+              alt="image"
+            />
           </div>
         </div>
       </section>
